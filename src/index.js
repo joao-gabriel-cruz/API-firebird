3d52cd0fe2c1c2e6378ca642f69317e334b26d14
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-05-19 10:29:34 -0300

    node-firebird instalado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const port = 7777
 
-app.listen(7777)
+app.use(express.json())
+
+app.listen(port,()=>{
+    console.log(`API rodando na porta ${port}...`);
+})
 
 app.get('/', (req,res) => res.send('hello'))
+
